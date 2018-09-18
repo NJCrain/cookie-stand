bae8070abe741e89d81ff77fdfd1c093b8276ab5
Nicholas Crain <nicholascrain@gmail.com>
2018-09-18 14:15:43 -0700

    added code for creating table head and restrucured beginning of render method to write location name at start of row

--- js/app.js
+++ js/app.js
@@ -18,16 +18,27 @@ var times = [
   '8PM'
 ];
 
+var newRow;
+var row;
+var newCell;
+var text;
+
 var Store = function(min, max, avg, name) {
   this.name = name;
   this.customerMin = min;
   this.customerMax = max;
   this.avgCookies = avg;
   this.hourlyTotals = [];
+  Store.stores.push(this);
 };
 
+Store.stores = [];
+
 Store.prototype.customers = function() {
-  return Math.floor(Math.random() * (this.customerMax - this.customerMin +1)) + this.customerMin;
+  return (
+    Math.floor(Math.random() * (this.customerMax - this.customerMin + 1)) +
+    this.customerMin
+  );
 };
 
 Store.prototype.cookies = function() {
@@ -39,26 +50,43 @@ Store.prototype.cookies = function() {
 Store.prototype.render = function() {
   this.cookies();
   var storeTotal = 0;
-  var list = document.getElementById(this.name);
-  var newLi;
-  var text;
+  var body = document.querySelector('tbody');
+  newRow = document.createElement('tr');
+  body.appendChild(newRow);
+  row = body.lastChild;
+  newCell = document.createElement('th');
+  text = document.createTextNode(this.name);
+  newCell.appendChild(text);
+  row.appendChild(newCell);
   for (var i = 0; i < this.hourlyTotals.length; i++) {
     storeTotal += this.hourlyTotals[i];
-    text = times[i] + ': ' + this.hourlyTotals[i];
-    newLi = document.createElement('li');
-    newLi.textContent = text;
-    list.appendChild(newLi);
+    row = body.lastChild;
+    text = document.createTextNode(this.hourlyTotals[i]);
+    newCell = document.createElement('td');
+    newCell.appendChild(text)
   }
-  newLi = document.createElement('li');
-  newLi.textContent = 'Total: ' + storeTotal;
-  list.appendChild(newLi);
 };
 
-var firstAndPike = new Store(23, 65, 6.3, 'firstAndPike');
-var seatacAirport = new Store(3, 24, 1.2, 'seatacAirport');
-var seattleCenter = new Store(11, 38, 3.7, 'seattleCenter');
-var capitolHill = new Store(20, 38, 2.3, 'capitolHill');
-var alki = new Store(2, 16, 4.6, 'alki');
+var firstAndPike = new Store(23, 65, 6.3, 'First And Pike');
+var seatacAirport = new Store(3, 24, 1.2, 'Seatac Airport');
+var seattleCenter = new Store(11, 38, 3.7, 'Seattle Center');
+var capitolHill = new Store(20, 38, 2.3, 'Capitol Hill');
+var alki = new Store(2, 16, 4.6, 'Alki');
+
+var head = document.querySelector('thead');
+newRow = document.createElement('tr');
+head.appendChild(newRow);
+row = head.lastChild;
+newCell = document.createElement('th');
+row.appendChild(newCell);
+
+for (var i = 0; i < times.length; i++) {
+  newCell = document.createElement('th');
+  text = document.createTextNode(times[i]);
+  newCell.appendChild(text);
+  row.appendChild(newCell);
+}
+
 firstAndPike.render();
 seatacAirport.render();
 seattleCenter.render();
