5a1c5319c1735fc22935a8c168d91d7843716052
Nicholas Crain <nicholascrain@gmail.com>
2018-09-18 14:43:14 -0700

    added section to create total column in header and code to show the total in render. Wrapped header creation in a function

--- js/app.js
+++ js/app.js
@@ -60,11 +60,15 @@ Store.prototype.render = function() {
   row.appendChild(newCell);
   for (var i = 0; i < this.hourlyTotals.length; i++) {
     storeTotal += this.hourlyTotals[i];
-    row = body.lastChild;
     text = document.createTextNode(this.hourlyTotals[i]);
     newCell = document.createElement('td');
-    newCell.appendChild(text)
+    newCell.appendChild(text);
+    row.appendChild(newCell);
   }
+  newCell = document.createElement('td');
+  text = document.createTextNode(storeTotal);
+  newCell.appendChild(text);
+  row.appendChild(newCell);
 };
 
 var firstAndPike = new Store(23, 65, 6.3, 'First And Pike');
@@ -73,20 +77,28 @@ var seattleCenter = new Store(11, 38, 3.7, 'Seattle Center');
 var capitolHill = new Store(20, 38, 2.3, 'Capitol Hill');
 var alki = new Store(2, 16, 4.6, 'Alki');
 
-var head = document.querySelector('thead');
-newRow = document.createElement('tr');
-head.appendChild(newRow);
-row = head.lastChild;
-newCell = document.createElement('th');
-row.appendChild(newCell);
-
-for (var i = 0; i < times.length; i++) {
+var createHeader = function() {
+  var head = document.querySelector('thead');
+  newRow = document.createElement('tr');
+  head.appendChild(newRow);
+  row = head.lastChild;
   newCell = document.createElement('th');
-  text = document.createTextNode(times[i]);
+  row.appendChild(newCell);
+
+  for (var i = 0; i < times.length; i++) {
+    newCell = document.createElement('th');
+    text = document.createTextNode(times[i]);
+    newCell.appendChild(text);
+    row.appendChild(newCell);
+  }
+
+  newCell = document.createElement('th');
+  text = document.createTextNode('Total');
   newCell.appendChild(text);
   row.appendChild(newCell);
-}
+};
 
+createHeader();
 firstAndPike.render();
 seatacAirport.render();
 seattleCenter.render();
